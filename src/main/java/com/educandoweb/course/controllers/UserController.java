a3b0620f217d4f5f408fb3d5b834fa08f02306bb
DouglasVulcano <vulcanodouglas1708@gmail.com>
2024-06-29 21:00:23 -0300

    service layer, component registration

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -1,17 +1,31 @@
 package com.educandoweb.course.controllers;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Douglas", "vulcanodouglas1708@gmail.com", "111111111", "password");
-        return ResponseEntity.status(HttpStatus.OK).body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> usersList = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(usersList);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
